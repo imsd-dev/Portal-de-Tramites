e599b4a57a524d2df4addd6dc7b0905d1c9237f3
Nicolas Carreño <nicocarrenoh@gmail.com>
2019-02-08 17:03:15 -0300

    cambios Header

--- public/templates/header.php
+++ public/templates/header.php
@@ -1,4 +1,4 @@
-	 	
+<link rel="stylesheet" href="<?php echo URL ?>public/vendor/bootstrap/css/bootstrap.min.css">	 	
 <div class="container" style="margin-top:5px;">
 <div class="pull-right hidden-xs">
 
@@ -7,48 +7,50 @@
  
 
 <header id="header" class="hidden-print" >
-
     <div class="d-none">
-        <div class="header-container container">
-         
+        <div class="header-container container">  
 		    <div class="header-row">
-			
                 <div class="header-column" style="padding-top:35px;">
 					<div class="headerLogo">
                         <a href="<?php echo URL ?>">
                             <img alt="Municipalidad de Santo Domingo" src="<?php echo URL ?>public/img/logo.png">
                         </a>
-                    </div>	
-
-                    <div class="container" >
-						<div class="pull-right hidden-xs">
-						
-							<a target="_blank" href="https://www.leylobby.gob.cl/instituciones/MU309" data-wpel-link="external" rel="external noopener noreferrer"><img alt="Audiencia Lobby" src="http://tramites.santodomingo.cl/social/lobby.png"></a>   	 
-							<a target="_blank" href="https://www.portaltransparencia.cl/PortalPdT/web/guest/directorio-de-organismos-regulados?p_p_id=pdtorganismos_WAR_pdtorganismosportlet&orgcode=3f196a4c53edca8a75e1f274ee572221" data-wpel-link="external" rel="external noopener noreferrer"><img alt="Solicitud de Transparencia" src="http://tramites.santodomingo.cl/social/solicitud.png"></a>    	 	
-							<a target="_blank" href="https://www.portaltransparencia.cl/PortalPdT/pdtta?codOrganismo=MU309" data-wpel-link="external" rel="external noopener noreferrer"><img alt="Transparencia activa" src="http://tramites.santodomingo.cl/social/ley-activa.png"></a> 
-						
+					</div>
+					<div class="container hidden-xs" >
+		 				<section class= "offset-md-6 hidden-sm" id="contenedor-leyes">
+							 <ul style="list-style-type:none">
+								 <li>
+								 	<a target="_blank" href="https://www.leylobby.gob.cl/instituciones/MU309" data-wpel-link="external" rel="external noopener noreferrer">
+									<img alt="Audiencia Lobby" src="http://tramites.santodomingo.cl/social/lobby.png"></a>  
+								 </li>
+								 <li>
+								 	<a target="_blank" href="https://www.portaltransparencia.cl/PortalPdT/web/guest/directorio-de-organismos-regulados?p_p_id=pdtorganismos_WAR_pdtorganismosportlet&orgcode=3f196a4c53edca8a75e1f274ee572221" data-wpel-link="external" rel="external noopener noreferrer">
+								 	<img alt="Solicitud de Transparencia" src="http://tramites.santodomingo.cl/social/solicitud.png"></a>  
+								 </li>
+								 <li>
+									<a target="_blank" href="https://www.portaltransparencia.cl/PortalPdT/pdtta?codOrganismo=MU309" data-wpel-link="external" rel="external noopener noreferrer">
+									<img alt="Transparencia activa" src="http://tramites.santodomingo.cl/social/ley-activa.png"></a> 
+								 </li>
+							 </ul>
+						</section>
+						<div class="pull-right acces1">
+							<div class="redes-sociales">
+								<a href="https://www.facebook.com/santodomingocl/" rel="external noopener noreferrer nofollow">
+								<img src="<?php echo URL ?>social/facebook.png" alt="Nuestro Facebook" title="Facebook" > </a>
+								<a href="https://twitter.com/santodomingocl" rel="external noopener noreferrer nofollow">
+								<img src="<?php echo URL ?>social/twitter.png" alt="Nuestro Twitter" title="Twitter" > </a>
+								 <a href="https://www.youtube.com/user/santodomingopuntocl"  rel="external noopener noreferrer nofollow">
+								<img src="<?php echo URL ?>social/youtube.png" alt="Nuestro Youtube" title="Youtube" > </a>
+							</div>	
 						</div>
-						
-						<br />
-						<br />
-						
-						<div class="pull-right"> <!-- style="width:500px;"-->
-							
-							<br />
-														
-							<div class="col-md-12">
-							
-								<!--div class="acces1 pull-right"-->
-								<div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve pull-right">
-									<a rel="nofollow" class="rsbtn_play" accesskey="L" title="Escucha esta p&aacute;gina utilizando ReadSpeaker"   href="//app-na.readspeaker.com/cgi-bin/rsent?customerid=8023&lang=es_us&readid=leer&url=">
-										<span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Escuchar</span></span></span>
-										<span class="rsbtn_right rsimg rsplay rspart"></span>
-									</a>
-								</div>
-								
-								<br />
-								<br />
-												 
+					</div>	
+				</div>
+			</div>
+		</div>
+		
+	</div>	
+
+								<!-- acces1-->									 
 								<!--div class="acces1">
 								 
 									<a href="#" onclick="zoomIn()"   style="float:left; margin-right:10px;">	
@@ -71,28 +73,14 @@
 								
 								</div><!-- col-md-6-->
 											
-								<div class="acces1 pull-right">
-					
-									 <a href="https://www.facebook.com/santodomingocl/" rel="external noopener noreferrer nofollow"><img src="<?php echo URL ?>social/facebook.png" alt="Nuestro Facebook" title="Facebook" > </a>
-									 
-									 <a href="https://twitter.com/santodomingocl" rel="external noopener noreferrer nofollow"><img src="<?php echo URL ?>social/twitter.png" alt="Nuestro Twitter" title="Twitter" > </a>
-									 
-									 <a href="https://www.youtube.com/user/santodomingopuntocl"  rel="external noopener noreferrer nofollow"><img src="<?php echo URL ?>social/youtube.png" alt="Nuestro Youtube" title="Youtube" > </a>
-					 
-								</div><!-- acces1-->
 								
-							</div><!-- col-md-6-->	
-							
-						</div> <!-- / col-md-12 -->	
-						
-					</div>
-					
-                </div>
+								
+							<!-- col-md-6-->	
                 
-            </div>
+            
 			 
 		<!-- READSPEARKER -->	
-				<div class="row" >
+				<!-- <div class="row" >
 					<div class="col-md-12">
 						<div class="container mb20">
 							<div class="col-md-12">
@@ -101,14 +89,14 @@
 									
 								
 							</div> <!-- / col-md-12 -->
-						</div> <!-- / container -->
-					</div> <!-- / container -->
-				</div> <!-- / container -->
+						<!-- </div> / container -->
+					<!-- </div> / container -->
+				<!-- </div> / container -->
 
 
 
-		</div>
-	</div>
+		<!-- </div>
+	</div> --> 
 
 
 </header>
